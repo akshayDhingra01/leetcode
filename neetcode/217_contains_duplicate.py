d0b999ae3b0814019c84afce0a34d715538d02df
akshayDhingra01 <a4akshayd@gmail.com>
2023-12-21 23:41:35 +0530

    ADD: solution to the question contains duplicate

---
+++ neetcode/217_contains_duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        lengthOfList = len(nums)
+        lengthOfSet = len(set(nums))
+        if (lengthOfSet < lengthOfList):
+            return True
+        else:
+            return False
