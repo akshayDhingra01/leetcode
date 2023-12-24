56ef88aecc14dff4cc7ea0bbbda40dd85528117b
akshayDhingra01 <a4akshayd@gmail.com>
2023-12-24 22:57:47 +0530

    ADD: question's solution added

--- neetcode/347_top_k_elements.py
+++ neetcode/347_top_k_elements.py
@@ -1,1 +1,22 @@
 # https://leetcode.com/problems/top-k-frequent-elements/
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        nums_dict = {}
+        for number in nums:
+            if number not in nums_dict.keys():
+                nums_dict[number] = 1
+            else:
+                nums_dict[number] += 1
+
+        nums_dict_sorted = sorted(nums_dict.items(), key=lambda x: x[1], reverse=False)
+        print(nums_dict_sorted)
+        return_array = []
+        for key in nums_dict_sorted:
+            return_array.append(key[0])
+            if k == 0:
+                break
+            k -= 1
+        if len(return_array) == 0:
+            return nums
+        return return_array
