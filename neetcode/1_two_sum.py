ce71f319d78f347d3637ff2655051bdd62739310
akshayDhingra01 <a4akshayd@gmail.com>
2023-12-26 22:51:45 +0530

    ADD: second solution to two sum

--- neetcode/1_two_sum.py
+++ neetcode/1_two_sum.py
@@ -14,3 +14,18 @@ class Solution:
                 end_pointer -= 1
             else:
                 start_pointer += 1
+
+
+
+
+# Other Approach
+                
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}  # val -> index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
