63ed487aded6f55349e04b77207137f6ea699e14
akshayDhingra01 <a4akshayd@gmail.com>
2023-12-28 23:09:20 +0530

    ADD: solution to two sum 2

--- neetcode/two-pointers/0167-two-sum-2-input-array-is-sorted.py
+++ neetcode/two-pointers/0167-two-sum-2-input-array-is-sorted.py
@@ -1,1 +1,15 @@
 # https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        start_pointer = 0 
+        last_pointer = len(numbers) - 1
+
+        while start_pointer != last_pointer:
+            sumOfTarget = numbers[start_pointer] + numbers[last_pointer]
+            if sumOfTarget == target:
+                return  [start_pointer + 1, last_pointer + 1]
+            elif sumOfTarget > target:
+                last_pointer -= 1
+            else:
+                start_pointer += 1
