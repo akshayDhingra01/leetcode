51f721c4f86536da7ecea040cfcb92121bf5b0e5
akshayDhingra01 <a4akshayd@gmail.com>
2023-12-29 17:55:29 +0530

    ADD: solution to buy and sell stock

--- neetcode/slding-window/0121-best-time-to-buy-and-sell-stock.py
+++ neetcode/slding-window/0121-best-time-to-buy-and-sell-stock.py
@@ -1,1 +1,15 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        min_side = prices[0]
+        max_profit = 0
+
+        for price in prices[1:]:
+            if price < min_side:
+                min_side = price
+            if price > min_side:
+                max_profit = max(price - min_side, max_profit)       
+
+        return max_profit
