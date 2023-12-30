bc7c4838bde5a159937255b18066588b1cdebb6a
akshayDhingra01 <a4akshayd@gmail.com>
2023-12-30 23:03:21 +0530

    ADD: Question link

--- leetcode_streak/1897-redistribute-characters.py
+++ leetcode_streak/1897-redistribute-characters.py
@@ -1,3 +1,5 @@
+# https://leetcode.com/problems/redistribute-characters-to-make-all-strings-equal/?envType=daily-question&envId=2023-12-30
+
 class Solution:
     def makeEqual(self, words: List[str]) -> bool:
         counts = {}
