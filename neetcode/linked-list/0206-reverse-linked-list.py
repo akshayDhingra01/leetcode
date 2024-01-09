16bf6a4d0d169bd86d58822a70ef81e4141a83aa
akshayDhingra01 <a4akshayd@gmail.com>
2024-01-09 16:36:59 +0530

    ADD: time and space

--- neetcode/linked-list/0206-reverse-linked-list.py
+++ neetcode/linked-list/0206-reverse-linked-list.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/reverse-linked-list/
 
+# First Optimized Solution || Time O(N) || Space O(1) || Linked List
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
