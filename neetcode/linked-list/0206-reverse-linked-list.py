e642b98c1d19f32b301ac5bcee601178d99d75bc
akshayDhingra01 <a4akshayd@gmail.com>
2024-01-09 16:36:19 +0530

    ADD: FIrst solution

--- neetcode/linked-list/0206-reverse-linked-list.py
+++ neetcode/linked-list/0206-reverse-linked-list.py
@@ -1,1 +1,20 @@
 # https://leetcode.com/problems/reverse-linked-list/
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        if head is None:
+            return None
+        if head.next is None:
+            return head
+        previousNode = head
+        curr = head.next
+        head.next = None
+        while curr.next != None:
+            newCurr = curr.next
+            curr.next = previousNode
+            previousNode = curr
+            curr = newCurr
+
+        curr.next = previousNode
+        return curr
